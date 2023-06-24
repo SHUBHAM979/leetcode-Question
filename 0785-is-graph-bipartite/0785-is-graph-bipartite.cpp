e03e8baf2572a83cf13cd9c30316ba35e0eef3ff
SleepY <76165571+SHUBHAM979@users.noreply.github.com>
2023-06-24 18:59:59 +0530

    Time: 38 ms (8.49%), Space: 13.9 MB (20.85%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,32 +1,50 @@
 class Solution {
 public:
+    void dfs(int i,map<int,bool>& mp, vector<int>& vis ,vector<vector<int>>& graph, bool& flag) {
+        vis[i] =1;
+        for(auto j : graph[i]) {
+            if(!vis[j]) {
+                mp[j] = !mp[i]; 
+                        dfs(j, mp, vis, graph,flag);
+                        
+                    } else {
+                        if(mp[j] == mp[i]) { 
+                           // cout << x << " ." << j << mp[j]<< " " << mp[1] << mp[3];
+                            flag =  false; }
+                    }
+                }
+        
+    }
+    
     bool isBipartite(vector<vector<int>>& graph) {
         map<int, bool > mp;
         vector<int> vis(graph.size(), 0);
         for(int i =0; i< graph.size(); i++) {
             if(vis[i]) continue;
-            queue<int> qu;
-            qu.push(i);
+            // queue<int> qu;
+            // qu.push(i);
             bool flag =true;
             mp[i] = true;
-            while(!qu.empty()) {
+            dfs(i,mp,vis,graph, flag);
+            if(flag == false) return false;
+//             while(!qu.empty()) {
                 
-                int x = qu.front();
-                qu.pop();
-                vis[x] =1;
-               flag = !mp[x];
-                for(auto j : graph[x]) {
-                    if(!vis[j]) {
-                        qu.push(j);
-                        mp[j] =flag; 
-                    } else {
-                        if(mp[j] != flag) { 
-                            cout << x << " ." << j << mp[j]<< " " << mp[1] << mp[3];
-                            return false; }
-                    }
-                }
+//                 int x = qu.front();
+//                 qu.pop();
+//                 vis[x] =1;
+//                flag = !mp[x];
+//                 for(auto j : graph[x]) {
+//                     if(!vis[j]) {
+//                         qu.push(j);
+//                         mp[j] =flag; 
+//                     } else {
+//                         if(mp[j] != flag) { 
+//                             cout << x << " ." << j << mp[j]<< " " << mp[1] << mp[3];
+//                             return false; }
+//                     }
+//                 }
                 
-            }
+           // }
             
         }
         return true;
