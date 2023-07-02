b11be6c0e2c9a7acbe06c2494f33f9ff60fcc681
SleepY <76165571+SHUBHAM979@users.noreply.github.com>
2023-07-02 10:33:46 +0530

    Time: 7 ms (70.93%), Space: 11.4 MB (42.73%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.cpp
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -1,29 +1,42 @@
 class Solution {
 public:
-  void rec(int i , string s, set<string>& ans, string minians) {
+    void rec(int i,string s, string minians, vector<string> &ans){
+//         if(index==s.length()){
+//             ans.emplace_back(temp);
+//             return;
+//         }
+//         if(!isdigit(temp[index])){
+//           temp[index] = toupper(temp[index] );
+//           backtrack(index+1, s, temp, ans);
+//           temp[index] = tolower(temp[index]);
+//           backtrack(index+1, s, temp, ans);
+//         } else {
+// backtrack(index+1, s, temp, ans);
+//         }
+        
+        
         if(i == s.size()) {
-            ans.insert(minians);
+            ans.push_back(minians);
             return;
         }
+        if(isdigit(s[i])) {
+            minians.push_back(s[i]);
+            rec(i+1, s, minians,ans);
+            return;
+        }
+        
         char it = s[i];
         string m = minians;
         m += tolower(it);
         
-        rec(i+1,s , ans, m );
+        rec(i+1,s , m,ans );
         minians += toupper(it);
-        rec(i+1,s,ans, minians);
+        rec(i+1,s, minians, ans);
         
     }
-    
     vector<string> letterCasePermutation(string s) {
-        set<string> ans;
-        rec(0 ,s , ans, "");
-        vector<string> rans;
-        for(auto i : ans) {
-            rans.push_back(i);
-        }
-        
-        return rans;
-        
+        vector<string> ANS;
+        rec(0,s, "", ANS);
+        return ANS;
     }
 };
