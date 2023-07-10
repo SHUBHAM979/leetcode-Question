9118ee1e91d8b80706d63c982cd2cd5e6c09d5e4
SleepY <76165571+SHUBHAM979@users.noreply.github.com>
2023-07-10 21:19:06 +0530

    Time: 47 ms (13.99%), Space: 89.6 MB (10.88%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,12 +1,21 @@
 class Solution {
+    vector<int> dp;
+    
 public:
+    Solution() : dp(1000001,-1) {
+        
+    }
+    
     int numTrees(int n) {
         if(n  <= 1) return 1;
         int ans = 0;
+        if(dp[n] != -1) {
+            return dp[n];
+        }
         for(int i=1; i<=n ;i++) {
             ans += numTrees(i-1) * numTrees(n- i);
         }
-        return ans;
+        return dp[n] = ans;
     }
 
 };
