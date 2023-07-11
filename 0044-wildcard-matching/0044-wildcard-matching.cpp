a29dc4e84b9e1fff746f6c35b1ded8eb2eb6afc9
SleepY <76165571+SHUBHAM979@users.noreply.github.com>
2023-07-12 05:29:12 +0530

    Time: 1529 ms (5.02%), Space: 780.9 MB (5.03%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+//     bool matching(int i , int j, string s, string p,  vector<vector<int>>& dp) {
+//         if( i == s.length() && j == p.length()) {
+//             return true;
+//         }
+//         if((i >= s.length() && j <p.length()) &&  (p[j] !='*') ) {
+//             return false;
+//         }
+//         if(j >= p.length() && i < s.length()) {
+//             return false;
+//         }
+//         if(dp[i][j] != -1) {
+//             if(dp[i][j] == 1) return true;
+//             else return false;
+//         }
+      
+//         if(s[i] == p[j] || p[j] =='?' ) {
+//              return dp[i][j] =  matching(i+1, j+1, s, p,dp );
+//         } else if (p[j] == '*') {
+//                 if( i== s.length()) {
+//                       return  dp[i][j]  = matching(i, j+1,s,p, dp); 
+                  
+//                 } else 
+//                      return  dp[i][j]  =( matching(i, j+1,s,p, dp) ||  matching(i+1, j,s,p,dp) || matching(i+1,j+1,s,p,dp)  );
+                  
+                
+            
+//         } 
+//         return dp[i][j] = 0;
+        
+        
+//     }
+    
+//     bool isMatch(string s, string p) {
+//         vector<vector<int>> dp(s.length() +1, vector<int>(p.length()+1, -1));
+//         return matching(0,0,s,p, dp);
+        
+//     }
+ 
+    bool isMatch(string s, string p) {
+        if(p.length()==0){
+            return (s.length()==0);
+        }
+        vector<vector<int>> v(s.length()+1,vector<int> (p.length()+1,-1));
+      return  helper(s,p,0,0,v);
+      
+    }
+    bool helper(string s, string p,int i,int j,vector<vector<int>> &v)
+    {
+        if(j==p.length())
+            return (i==s.length());
+        if(v[i][j]<0){
+        if(i==s.length())
+            v[i][j]= (p[j]=='*' && helper(s,p,i,j+1,v));
+        else if(i<s.length() &&  (p[j]==s[i] || p[j]=='?'))
+        {
+           v[i][j]=  helper(s,p,i+1,j+1,v);
+        }
+        
+        else if(p[j]=='*')
+        {
+            v[i][j]= (helper(s,p,i,j+1,v) || helper(s,p,i+1,j,v));
+        }
+        else
+        v[i][j]= false;
+        }
+        return v[i][j];
+    }
+};
