7c3d614e2c2029b41316a2e70c0887ddb4dbf5ef
SleepY <76165571+SHUBHAM979@users.noreply.github.com>
2023-07-13 00:45:15 +0530

    Time: 16 ms (72.68%), Space: 19.7 MB (92.11%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,21 +2,35 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n= height.size();
-        vector<int> left(n), right(n);
-        int lmaxi = INT_MIN , rmaxi =INT_MIN;
+        // vector<int> left(n), right(n);
+//         int lmaxi = INT_MIN , rmaxi =INT_MIN;
         
-        for(int i=0; i< n; i++) {
-            lmaxi = max(lmaxi, height[i]);
-            left[i] = lmaxi;
+//         for(int i=0; i< n; i++) {
+//             lmaxi = max(lmaxi, height[i]);
+//             left[i] = lmaxi;
             
-            rmaxi = max(rmaxi, height[n-1-i]);
-            right[n-1-i] = rmaxi;
+//             rmaxi = max(rmaxi, height[n-1-i]);
+//             right[n-1-i] = rmaxi;
             
-        }
+//         }
         
+//         int ans =0;
+//         for(int i=0; i< n; i++) {
+//             ans += min(left[i] , right[i]) -height[i];
+//         }
         int ans =0;
-        for(int i=0; i< n; i++) {
-            ans += min(left[i] , right[i]) -height[i];
+        int leftmax =0, rightmax =0;
+        int left =0, right = n-1;
+        while(left < right) {
+            if(height[left] <= height[right]) {
+                if(height[left] >= leftmax) leftmax = height[left];
+                else ans += leftmax - height[left];
+                left++;
+            } else {
+                  if(height[right] >= rightmax) rightmax = height[right];
+                else ans += rightmax - height[right];
+                right--;
+            }
         }
         
         return ans;
