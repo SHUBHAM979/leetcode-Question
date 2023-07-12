4d626e9d7b5d0ba9c21f54d33ffc72c81d83150b
SleepY <76165571+SHUBHAM979@users.noreply.github.com>
2023-07-12 06:08:03 +0530

    Time: 96 ms (34.82%), Space: 27.9 MB (23.47%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -36,8 +36,27 @@ public:
     
     bool isMatch(string s, string p) {
         if(p.length() == 0) return s.length() ==0;
-        vector<vector<int>> dp(s.length() +1, vector<int>(p.length()+1, -1));
-        return matching(0,0,s,p, dp);
+        // vector<vector<int>> dp(s.length() +1, vector<int>(p.length()+1, -1));
+        // return matching(0,0,s,p, dp);
+        vector<vector<int>> dp(s.length() +1, vector<int>(p.length()+1, 0));
+        
+         dp[0][0] = true;
+        for (int j = 0; j < p.size() && p[j] == '*'; ++j) {
+            dp[0][j + 1] = true;
+        }
+        
+        for(int i=1; i<= s.length(); i++) {
+            for(int j=1; j <= p.length(); j++) {
+               if(p[j-1] == '*') {
+                   dp[i][j] = dp[i][j-1] || dp[i-1][j];
+               } else {
+                   dp[i][j] =(s[i-1] == p[j-1] || p[j-1] == '?') && dp[i-1][j-1];
+               }
+            }
+        }
+        
+        return dp[s.length()][p.length()];
+        
         
     }
  
