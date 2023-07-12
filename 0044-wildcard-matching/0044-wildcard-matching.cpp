bac2829436acf88baa04fe93914bd66ca199adde
SleepY <76165571+SHUBHAM979@users.noreply.github.com>
2023-07-12 05:33:20 +0530

    Time: 1581 ms (5.02%), Space: 781.2 MB (5.03%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -41,33 +41,5 @@ public:
         
     }
  
-//     bool isMatch(string s, string p) {
-//         if(p.length()==0){
-//             return (s.length()==0);
-//         }
-//         vector<vector<int>> v(s.length()+1,vector<int> (p.length()+1,-1));
-//       return  helper(s,p,0,0,v);
-      
-//     }
-//     bool helper(string s, string p,int i,int j,vector<vector<int>> &v)
-//     {
-//         if(j==p.length())
-//             return (i==s.length());
-//         if(v[i][j]<0){
-//         if(i==s.length())
-//             v[i][j]= (p[j]=='*' && helper(s,p,i,j+1,v));
-//         else if(i<s.length() &&  (p[j]==s[i] || p[j]=='?'))
-//         {
-//            v[i][j]=  helper(s,p,i+1,j+1,v);
-//         }
-        
-//         else if(p[j]=='*')
-//         {
-//             v[i][j]= (helper(s,p,i,j+1,v) || helper(s,p,i+1,j,v));
-//         }
-//         else
-//         v[i][j]= false;
-//         }
-//         return v[i][j];
-//     }
+
 };
