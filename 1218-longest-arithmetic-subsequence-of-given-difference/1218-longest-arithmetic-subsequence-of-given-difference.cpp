5c106dca0fa9218164f51c6aa592cdb84446c486
SleepY <76165571+SHUBHAM979@users.noreply.github.com>
2023-07-15 02:43:07 +0530

    Time: 165 ms (55.68%), Space: 56.7 MB (20.80%) - LeetHub

--- 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.cpp
+++ 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.cpp
@@ -1,15 +1,35 @@
 class Solution {
 public:
-    int longestSubsequence(vector<int>& arr, int difference) {
-        unordered_map<int, int> dp;
-        int answer = 1;
-        
-        for (int a : arr) {
-            int beforeA = dp.count(a - difference) ? dp[a - difference] : 0;
-            dp[a] = beforeA + 1;
-            answer = max(answer, dp[a]);
+    // dp solution givng TLE
+    // int rec(int i , int j, vector<int>& arr, int d ,vector<vector<int>>& dp) {
+    //     if(arr.size() == i) return 0;\
+    //     if(dp[i+1][j+1] != -1) return dp[i+1][j+1];
+    //     if(j == -1) {
+    //         return  dp[i+1][j+1] =max(1+ rec(i+1, i, arr, d,dp), rec(i+1, j, arr, d,dp));
+    //     }
+    //     if(arr[i]- arr[j] == d) {
+    //         return dp[i+1][j+1] = 1+ rec(i+1, i, arr, d,dp);
+    //     }
+    //     return dp[i+1][j+1] = rec(i+1,j, arr,d,dp);
+    // }
+    
+    
+    int longestSubsequence(vector<int>& arr, int diff) {
+        // vector<vector<int>> dp(arr.size()+1, vector<int>(arr.size()+1,-1));
+        // return rec(0, -1, arr, difference, dp);
+        unordered_map<int,int> mp;
+        vector<int> dp(arr.size(),1);
+        for(int i =0; i< arr.size(); i++) {
+            if(mp.find(arr[i] - diff) != mp.end()) {
+                dp[i] += dp[mp[arr[i]-diff]];
+            }
+            mp[arr[i]] = i;
         }
+        int  maxi =INT_MIN;
+        for(auto i : dp) {
+            maxi = max(i, maxi);
+        }
+        return maxi;
         
-        return answer;
     }
-};
+}; 
