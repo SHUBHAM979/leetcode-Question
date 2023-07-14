5e04c73fb4e2522d54f1958c109e15101c2fb11a
SleepY <76165571+SHUBHAM979@users.noreply.github.com>
2023-07-15 03:20:09 +0530

    Time: 495 ms (43.19%), Space: 10.4 MB (84.98%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,28 +1,16 @@
 class Solution {
 public:
-  
-    int df(int i, int prev,  vector<int>& nums, vector<vector<int >> &dp) {
-        if(i==nums.size()) {
-            
-            return 0;
-        }
-        if(dp[i][prev+1] !=-1) return dp[i][prev+1];
-        
-          int y =0,len=0;
-       
-        if( prev ==-1 ||  nums[i] > nums[prev] ){
-               y =1+ df(i+1, i, nums,dp ) ;                          
-        } 
-    len = df(i+1,prev,nums,dp);
-        
-        return dp[i][prev+1] = max(len,y);;
-        
-    }
-    
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size(), vector<int>(nums.size()+1, -1));
-        
-        return df(0, -1,nums,dp);
-        
+        int n = nums.size();
+        vector<int> dp(n,1);int ans = 1;
+        for(int i=0; i< n; i++) {
+            for(int j =0; j<i; j++) {
+                if(nums[i] > nums[j]) {
+                    dp[i] = max(dp[i], dp[j]+1);
+                    ans = max(ans, dp[i]);
+                }
+            }
+        }
+        return ans;
     }
 };
