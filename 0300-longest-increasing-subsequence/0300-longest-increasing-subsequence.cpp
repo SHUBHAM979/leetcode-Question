c81980adbdd42f86851ebc62ecc506748c5443b1
SleepY <76165571+SHUBHAM979@users.noreply.github.com>
2023-07-22 12:06:45 +0530

    Time: 331 ms (64.04%), Space: 10.4 MB (67.00%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,17 +1,30 @@
 class Solution {
-    int  longest(int i, int prev, vector<int>& nums, vector<vector<int>>& dp) {
-        if(i == nums.size()) return 0;
-        if(dp[i][prev+1] != -1) return dp[i][prev+1];
-        if(prev == -1 || nums[i] > nums[prev]) {
-            return dp[i][prev+1] =  max(1 + longest(i+1, i, nums, dp), longest(i+1, prev, nums,dp));
-        }
-        return dp[i][prev+1] = longest(i+1,prev, nums,dp);
-    }
+    // int  longest(int i, int prev, vector<int>& nums, vector<vector<int>>& dp) {
+    //     if(i == nums.size()) return 0;
+    //     if(dp[i][prev+1] != -1) return dp[i][prev+1];
+    //     if(prev == -1 || nums[i] > nums[prev]) {
+    //         return dp[i][prev+1] =  max(1 + longest(i+1, i, nums, dp), longest(i+1, prev, nums,dp));
+    //     }
+    //     return dp[i][prev+1] = longest(i+1,prev, nums,dp);
+    // }
     
     
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size(),vector<int>(nums.size()+1,-1));
-        return longest(0,-1, nums, dp);
+        // vector<vector<int>> dp(nums.size(),vector<int>(nums.size()+1,-1));
+        // return longest(0,-1, nums, dp);
+         vector<int> dp(nums.size(),1);
+        for(int i =0; i< nums.size(); i++) {
+            for(int j = 0; j < i; j++) {
+                if(nums[i]>nums[j]) {
+                    dp[i] = max(dp[i], dp[j]+1);
+                }
+            }
+        }
+        int ans = 0;
+        for(int i =0; i< nums.size(); i++) {
+            ans = max(ans, dp[i]);
+        }
+        return ans;
     }
 };
