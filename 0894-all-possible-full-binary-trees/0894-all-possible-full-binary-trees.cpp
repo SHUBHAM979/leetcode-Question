96b67223224f1fc739694eb37e3b241c4c6e50ee
SleepY <76165571+SHUBHAM979@users.noreply.github.com>
2023-07-23 06:54:23 +0530

    Time: 92 ms (93.37%), Space: 27.9 MB (85.37%) - LeetHub

--- 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
@@ -10,50 +10,25 @@
  * };
  */
 class Solution {
-    // vector<vector<TreeNode*>> dp;
 public:
-//     Solution() : dp(21) {
-        
-//     }
-    
     vector<TreeNode*> allPossibleFBT(int n) {
         if(n%2 == 0) return {};
-//         vector<TreeNode*> res;
-//         if(n == 1) {
-//             res.push_back(new TreeNode(0));
-//             return res;
-//         }
-//         if(dp[n].size() !=0){
-//             return dp[n];
-//         }
-//         for(int i =1 ; i < n; i +=2) {
-//             vector<TreeNode*> leftSubtree = allPossibleFBT(i);
-//             vector<TreeNode*> rightSubtree = allPossibleFBT(n-1 - i);
-//             for(auto left : leftSubtree) {
-//                 for(auto right : rightSubtree) {
-//                     TreeNode* root = new TreeNode(0, left, right);
-//                     res.push_back(root);
-//                 }
-//             }
-            
-//         }
-//         return dp[n] = res;
-     vector<vector<TreeNode*>> dp(n+1);
+        vector<vector<TreeNode*>> dp(n+1);
         dp[1].push_back(new TreeNode(0));
-        for(int i = 3; i <= n; i+=2) {
-            for(int j = 1; j< i; j+=2) {
-                  vector<TreeNode*> leftSubtree = dp[j];
-                vector<TreeNode*> rightSubtree = dp[i-1 - j];
-                for(auto left : leftSubtree) {
-                    for(auto right : rightSubtree) {
-                        TreeNode* root = new TreeNode(0, left, right);
-                        dp[i].push_back(root);
+        for(int i = 3 ;i <=n ; i+=2) {
+            for(int j =1; j<i; j+=2) {
+                vector<TreeNode*> leftsub , rightsub;
+                leftsub = dp[j];
+                rightsub = dp[i-j-1];
+                for(auto left : leftsub) {
+                    for(auto right : rightsub) {
+                        TreeNode* head = new TreeNode(0, left, right);
+                        dp[i].push_back(head);
                     }
                 }
             }
+            
         }
         return dp[n];
-        
-        
     }
 };
